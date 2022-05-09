d7aa05669981eff862e8aee7a9a0771cba83bed3
Titi404 <tmilford404@gmail.com>
2022-05-09 16:23:44 -0400

    (っ °Д °;)っ

--- JobTests/UnitTest1.cs
+++ JobTests/UnitTest1.cs
@@ -1,15 +1,71 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TechJobsOO;
 
 namespace JobTests
 {
     [TestClass]
-    public class UnitTest1
+    public class JobTests
     {
-        [TestMethod]
-        public void TestMethod1()
-        {
-            
 
+
+        [TestMethod]
+        public void EmptyTest() //test one blank
+        {
+            Assert.AreEqual(0, 0, .001);
         }
+        //do 4 ToString() unit tests here  - using toString method from the supporting classes from over there >>>>>>
+
+        [TestMethod]
+        public void NameTest()
+        {
+            Employer test_employer = new Employer("PokemonCodingSchool");
+            Assert.AreEqual("PokemonCodingSchool", test_employer.ToString());
+
+            Location test_location = new Location("PokeVerse");
+            Assert.AreEqual("PokeVerse", test_location.ToString());
+
+            PositionType test_PositionType = new PositionType("PokeChef");
+            Assert.AreEqual("PokeChef", test_PositionType.ToString());
+
+            CoreCompetency test_CoreCompetency = new CoreCompetency("PokeKnife");
+            Assert.AreEqual("PokeKnife", test_CoreCompetency.ToString());
+        }
+        [TestMethod]
+        public void BlankSpacesTest()
+        {
+            Job test_job = new Job("Product tester", new Employer("ACME"), new Location("Desert"), new PositionType("Quality control"), new CoreCompetency("Persistence"));
+            string testString = test_job.ToString();
+            char firstChar = testString[0];
+            char secondChar = testString[testString.Length - 1];
+            Assert.IsTrue(firstChar == secondChar);
+        }
+
+        [TestMethod]
+        public void JobTestID()
+        {
+            Job test_job = new Job();
+            Job test_job2 = new Job();
+            Assert.AreEqual(2, test_job.Id);
+        }
+
+
+        [TestMethod]
+        public void ValueNullTest()
+        {
+            Job job3 = new Job("Ice cream tester", new Employer(""), new Location("Home"), new PositionType("UX"), new CoreCompetency("Tasting ability"));
+            Assert.AreEqual("\nID: 4\nName: Ice cream tester\nEmployer: Data not available\nLocation: Home\nPosition Type: UX\nCore Competency: Tasting ability\n", job3.ToString());
+        }
+
+        //[TestMethod]
+        //public void WDFFF()
+        //{
+        //    Job testingjob = new Job("ABC", new Employer("ABC"), new Location("Denver"), new PositionType("Spelling Bees Master"), new CoreCompetency("Google and Dictionary"));
+
+
+
+
+        
+
     }
 }
+
--- TechJobsOO/Job.cs
+++ TechJobsOO/Job.cs
@@ -6,7 +6,7 @@ namespace TechJobsOO
         public int Id { get; }
         private static int nextId = 1;
 
-        
+
         public string Name { get; set; }
         public Employer EmployerName { get; set; }
         public Location EmployerLocation { get; set; }
@@ -14,7 +14,60 @@ namespace TechJobsOO
         public CoreCompetency JobCoreCompetency { get; set; }
 
         // TODO: Add the two necessary constructors.
+        public Job()
+        {
+            Id = nextId;
+            nextId++;
+        }
+
+        public Job(string name, Employer employerName, Location employerLocation, PositionType employerPositionType, CoreCompetency employerCoreCompetency) : this()
+        {
+            Name = name;
+            EmployerName = employerName;
+            EmployerLocation = employerLocation;
+            JobType = employerPositionType;
+            JobCoreCompetency = employerCoreCompetency;
+        }
 
         // TODO: Generate Equals() and GetHashCode() methods.
+        public override bool Equals(object obj)
+        {
+            return obj is Job job &&
+                   Id == job.Id;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(Id);
+        }
+        public override string ToString()
+        {
+            if (Name == "" || Name == null)
+            {
+                return "Oopz";
+            }
+            else
+            {
+                if (EmployerName.Value == "" || EmployerName.Value == null)
+                {
+                    EmployerName.Value = "Data not available";
+                }
+                if (EmployerLocation.Value == "" || EmployerLocation.Value == null)
+                {
+                    EmployerLocation.Value = "Data not available";
+                }
+                if (JobType.Value == "" || JobType.Value == null)
+                {
+                    JobType.Value = "Data not available";
+                }
+                if (JobCoreCompetency.Value == "" || JobCoreCompetency.Value == null)
+                {
+                    JobCoreCompetency.Value = "Data not available";
+                }
+
+
+                return $"\nID: {Id}\nName: {Name}\nEmployer: {EmployerName}\nLocation: {EmployerLocation}\nPosition Type: {JobType}\nCore Competency: {JobCoreCompetency}\n";
+            }
+        }
     }
 }
